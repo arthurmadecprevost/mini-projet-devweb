66e9422f72ba7440bfdadf4f40be40c70d6bd97f
Arthur Madec-Prévost <arthur.madecprevost@protonmail.com>
2021-02-26 23:12:16 +0100

    DefaultController is a lucky number generator.

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,21 @@
+<?php
+namespace App\Controller;
+
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+
+class DefaultController extends AbstractController
+{
+    /**
+     * @Route("/")
+     */
+    public function number(): Response
+    {
+        $number = random_int(0, 100);
+
+        return $this->render('lucky/number.html.twig', [
+            'number' => $number,
+        ]);
+    }
+}
