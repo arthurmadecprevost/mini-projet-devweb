1b18d3e81a076fd4cdc54af3b1cba08510e352fe
Matthias Robert <matthias.rbt120@gmail.com>
2021-03-02 16:53:03 +0100

    Ajout form create evenement

--- src/Controller/EvenementController.php
+++ src/Controller/EvenementController.php
@@ -3,7 +3,11 @@
 namespace App\Controller;
 
 use App\Entity\Evenement;
+use App\Form\EvenementType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -19,6 +23,28 @@ class EvenementController extends AbstractController
         return $this->render('evenement/index.html.twig', [
             'controller_name' => 'EvenementController',
             'evenements' => $evenements,
+        ]);
+    }
+
+    /**
+     * Créer un nouvel evenement.
+     * @Route("/nouvel-evenement", name="evenement.create")
+     * @param Request $request
+     * @param EntityManagerInterface $em
+     * @return RedirectResponse|Response
+     */
+
+    public function create(Request $request, EntityManagerInterface $em) : Response {
+        $evenement = new Evenement();
+        $form = $this->createForm(EvenementType::class, $evenement);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em->persist($evenement);
+            $em->flush();
+            return $this->redirectToRoute('events');
+        }
+        return $this->render('evenement/create.html.twig', [
+            'form' => $form->createView(),
         ]);
     }
 }
