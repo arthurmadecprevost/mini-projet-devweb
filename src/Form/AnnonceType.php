5d52ebbf9c7f3d0190faa60b63050e7fbb1114b8
Arthur Madec-Prévost <arthur.madecprevost@protonmail.com>
2021-03-19 15:23:18 +0100

    modification des relations entre entitées

--- src/Form/AnnonceType.php
+++ src/Form/AnnonceType.php
@@ -4,9 +4,12 @@ namespace App\Form;
 
 use App\Entity\Annonce;
 use App\Entity\Membre;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -15,6 +18,10 @@ class AnnonceType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
+            ->add('titre', TextType::class, [
+                'label' => 'Titre',
+                'attr' => ['class' => 'form-control']
+            ])
             ->add('contenu', TextareaType::class,[
                 'label' => 'Annonce',
                 'attr' => ['class' => 'form-control']
